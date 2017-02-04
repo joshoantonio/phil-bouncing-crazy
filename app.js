8d280b30dde23307c0f6295792b74a31c8e8fe01
Chris Fox <Chris Fox>
2017-02-04 02:38:40 +0000

    favicon

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'swficon.png')));
+app.use(favicon(path.join(__dirname, 'public', 'sfwicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
