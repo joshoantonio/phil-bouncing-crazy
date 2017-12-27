a476ebad5f35f609fb38ae64008387e11178156a
cfoxeu <confoxt@gmail.com>
2017-12-27 00:16:07 +0000

    fixes and changes

--- routes/index.js
+++ routes/index.js
@@ -39,5 +39,8 @@ router.get('/slides', function(req, res) {
 router.get('/venues', function(req, res) {
   res.render('venues', { title: 'Venues' })
 });
+router.get('/admin', function(req, res) {
+    res.render('admin', { title: 'Admin' })
+});
 
 module.exports = router;
