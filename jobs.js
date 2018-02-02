14a0ece7696414437f76cc69b0aac717f88fb655
Chris Fox <chris.fox@mpjdesign.ltd.uk>
2018-02-02 17:26:55 +0000

    new features 2018

---
+++ jobs.js
@@ -0,0 +1,46 @@
+let cards = [
+    { job: 'weak <h*> tags, recommend adding "in plymouth..."' },
+    { job: 'google map location and Facebook integration' },
+    { job: 'youtube video integration (embed)' },
+    { job: 'compare keywords to new list' },
+    { job: 'Kids Only => "Kids Bouncy Castle Hire Plymouth and across Devon" && Content provided' },
+    { job: 'Enquire now button for product item listings, send ref to Phil' }, // Each unit has a link to my contact page with enquire now on it (hidden form)
+    { job: 'Blog, requireds add, edit, and delete functionality (custom urls)' },
+    { job: 'Bluetooth DJ addon with Disco lights = £60' },
+    { job: 'Landing page image => Carousel 4 images on ~5s each' }, // The main title photo to lap with some package deals and latest castle
+    { job: 'Fix Heroku => dedicated platform' },
+    { job: 'More images on landing page to break up text' },
+    { job: 'Rework product tiles to better represent "Bouncing Crazy" branding' },
+]
+
+// UPDATED CONTENT
+// Keywords:
+`
+bouncy castle hire, disco dome, sumo suits, kids play, Inflatable pillow,
+the dome plymouth, game plymouth, kids face painting,Plymouth bouncy castles, kidsplay, Mr bounce, Funtasia, didi car, bouncy castles for hire, ymca plymouth, hire bouncy castle, kids bouncy castle, disco dome hire, sumo suit, bouncy castle blower, adult bouncy castle hire, devon, woolwell centre, inflatable slide, didi car, inflatable sumo suits, didi cars, bouncy castle for hire, adultbox, bouncy castle hire cornwall, kam speakers, bouncy castle hire prices, face painting for kids, toddler bouncy castle, plymouth dome, hire a bouncy castle, bouncy slide hire, kids bouncy castles, face painters for hire, inflatable hire, indoor bouncy castle hire, princess bouncy castle hire,
+`
+`
+(Currently kids only)
+Kids Bouncy Castle Hire Plymouth and across Devon
+`
+`
+We at Bouncing Crazy Bouncy Castle Hire are passionate about making your experience the best imaginable. So we have a section that is kept only for Kids Bouncy Castle Hire. These are fantastic Castles for any occasion with all sorts of themes available at your choice. If you think your Child is too small for a Bouncy Castle we have a fantastic Soft Play set to hire.
+Why not book yours today?
+Starting from £50 with FREE delivery up to 10 miles around Plymouth, Saltash and across Devon.
+`
+`
+Adult Bouncy Castle Hire for Plymouth and across Devon
+Did you think that Bouncy Castles were just for kids well we have a great surprise for you with out Adults Bouncy Castle Hire From the super Bouncy SImpson Castle to the Castle Wedding Castle (With personalised themes)
+To one of my personal favourites the stunning Disco Dome with Lights and sound built in and Bluetooth connectable. You can't beat it to add the WOW factor to your event
+So make your booking today by going to our Contact Page and following one of the options there. All Castle are free delivery 10 Miles around Plymouth, Saltash and Ivybridge.
+`
+`
+Under WELCOME TO BOUNCING CRAZY
+Quality Bouncy Castle Hire Plymouth and across Devon
+Kids Package 1 from the photo from my facebook page £125 Any Kids only Bouncy Castle, Ball Pool, Soft Play and happy hopperz
+Celebration Castle on All age
+DIY Disco on Kids only page
+Ball pool picture £35
+DidiCar £25 for 4
+Happy Hopperz £20 for 4
+`
